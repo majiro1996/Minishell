69808176ab26d65a01cc81811b5da85e55d791d0
Manuel Jimenez Rodriguez <manujime@c3r4s4.42malaga.com>
2023-05-15 20:13:47 +0200

    split input

--- main.c
+++ main.c
@@ -6,19 +6,42 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 21:14:58 by manujime          #+#    #+#             */
-/*   Updated: 2023/05/09 22:18:54 by manujime         ###   ########.fr       */
+/*   Updated: 2023/05/15 19:01:58 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "minishell.h"
 
-int	main(void)
+//Search and launch the right executable (based on the PATH variable or using a
+//relative or an absolute path)
+void	ft_launch_executable(char **input, int argc, char *argv[], char *envp[])
+{
+	pid_t	pid;
+	int		status;
+
+	(void)argc;
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(input[0], argv, envp) == -1)
+			perror("minishell");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+		perror("minishell");
+	else
+	{
+		waitpid(pid, &status, WUNTRACED);
+	}
+}
+
+//this is the main function, it displays a prompt and waits for the user to
+//enter a command. It then reads the command and executes it. It loops until
+//the user presses ctrl-D or types exit.
+int	main(int argc, char *argv[], char *envp[])
 {
 	char	*input;
+	char	**parsed;
 
 	while (1)
 	{
@@ -29,7 +52,8 @@ int	main(void)
 			break ;
 		}
 		add_history(input);
-		printf("%s\n", input);
+		parsed = ft_split(input, ' ');
+		ft_launch_executable(parsed, argc, argv, envp);
 		free(input);
 	}
 	return (0);
---
+++ minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 16:53:52 by manujime          #+#    #+#             */
+/*   Updated: 2023/05/15 16:58:26 by manujime         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <string.h>
+# include <sys/wait.h>
+
+#endif
