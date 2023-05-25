3b787a57f2b801afd40942bcc1a8f64323862b56
silvericarus <agonzalezrosa@hotmail.com>
2023-05-25 12:08:05 +0200

    try adding sig_ign

--- signals.c
+++ signals.c
@@ -24,9 +24,13 @@ void	signal_handler(int signal, siginfo_t *info, void *uap)
 void	signal_setter(void)
 {
 	struct sigaction	sa;
+	struct sigaction	sa_ignore;
 
 	sa.sa_sigaction = &signal_handler;
+	sa_ignore.sa_handler = SIG_IGN;
+	sigemptyset(&sa_ignore.sa_mask);
+	sa_ignore.sa_flags = 0;
 	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGQUIT, &sa_ignore, NULL);
 	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGQUIT, &sa, NULL);
 }
