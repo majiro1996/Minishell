75ee77fa88c0e5d01998135157b6d0df5ddb4daa
Majiro <majiro1996@gmail.com>
2023-05-30 21:54:26 +0200

    input split working

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 18:27:37 by manujime          #+#    #+#             */
-/*   Updated: 2023/05/30 20:07:40 by manujime         ###   ########.fr       */
+/*   Updated: 2023/05/30 21:49:25 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,30 +61,21 @@ char	*ft_get_env(char *input, t_data *data)
 // 	data->list->content = new;
 // }
 
-//joins the content of all the nodes of the list into a single string
+//joins all the content strings of the input linked list
+//into a single string and returns it
 char	*ft_full_join(t_input *list)
 {
-	int		c;
-	int		k;
 	char	*new;
+	char	*tmp;
 
-	c = 0;
-	k = 0;
-	new = malloc(sizeof(char) * ft_strlen(list->content));
-	if (!new)
-		return (NULL);
+	new = ft_strdup("");
 	while (list)
 	{
-		while (list->content[c])
-		{
-			new[k] = list->content[c];
-			k++;
-			c++;
-		}
+		tmp = ft_strjoin(new, list->content);
+		free(new);
+		new = tmp;
 		list = list->next;
-		c = 0;
 	}
-	new[k] = '\0';
 	return (new);
 }
 
@@ -93,16 +84,15 @@ char	*ft_full_join(t_input *list)
 void	ft_parse(t_data *data)
 {
 	char	**input;
-	int		c;
 	char	*tmp;
 
-	c = 0;
 	ft_input_parse(data);
 	ft_search_and_replace(data);
 	tmp = ft_full_join(data->list);
-	printf("new: %s\n", tmp);
-	input = ft_split(tmp, ' ');
-	printf("tmp: %s\n", tmp);
+	//printf("new: %s\n", tmp);
+	input = ft_split(tmp, ' ');//
+	//printf("tmp: %s\n", tmp);
+	//ft_print_char_matrix(input);
 	if (!input)
 		return ;
 	data->input = input;
